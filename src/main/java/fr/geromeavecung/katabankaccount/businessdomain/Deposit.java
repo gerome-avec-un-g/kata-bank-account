b984ef0ce5b8b8cd003eb060c3e01840b2a8190e
parmentier.g <gerome.parmentier@gmail.com>
2022-10-05 17:11:05 +0200

    user can't create with a negative deposit

--- src/main/java/fr/geromeavecung/katabankaccount/businessdomain/Deposit.java
+++ src/main/java/fr/geromeavecung/katabankaccount/businessdomain/Deposit.java
@@ -3,9 +3,9 @@ package fr.geromeavecung.katabankaccount.businessdomain;
 public record Deposit(Amount amount) {
 
     public Deposit(Amount amount) {
-        //if (amount.value() <= 0) {
-        //  throw new IllegalArgumentException("Deposit amount can't be less or equal to 0");
-        //}
+        if (amount.value() < 0) {//TODO also forbid 0
+            throw new IllegalArgumentException("Deposit amount can't be less or equal to 0");
+        }
         this.amount = amount;
     }
 }
--- src/test/java/fr/geromeavecung/katabankaccount/usecases/AUserCreateABankAccountTest.java
+++ src/test/java/fr/geromeavecung/katabankaccount/usecases/AUserCreateABankAccountTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class AUserCreateABankAccountTest {
@@ -45,8 +46,17 @@ public class AUserCreateABankAccountTest {
 
     @Test
     void given_a_connected_user_when_he_create_an_account_with_a_first_deposit_of_negative_10_euros_then_he_obtains_an_error() {
-        fail();
-    }
+        User user = new User(UUID.fromString("29516229-e614-4f28-bdfb-ba77cd93e837"));
+        AccountsInMemory accountsInMemory = new AccountsInMemory();
+        AUserCreateABankAccount aUserCreateABankAccount = new AUserCreateABankAccount(new CreateAccount(accountsInMemory));
+        Deposit firstDeposit = new Deposit(new Amount(10));
+        OperationsHistory operationsHistory = new OperationsHistory(firstDeposit);
+        AccountCreationForm accountCreationForm = new AccountCreationForm(-10);
+
+        assertThatThrownBy(() -> aUserCreateABankAccount.execute(user, accountCreationForm))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Deposit amount can't be less or equal to 0");
+        }
 
     @Test
     void given_a_connected_user_with_an_account_when_he_create_a_second_account_then_he_obtains_an_error() {
