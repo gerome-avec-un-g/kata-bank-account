55c40adbf420d51cda3e1a3c66bbb9a7b8978b84
parmentier.g <gerome.parmentier@gmail.com>
2022-10-05 16:13:05 +0200

    adding first tests about account creation

--- src/test/java/fr/geromeavecung/katabankaccount/usecases/AUserCreateABankAccountTest.java
+++ src/test/java/fr/geromeavecung/katabankaccount/usecases/AUserCreateABankAccountTest.java
@@ -1,4 +1,24 @@
 package fr.geromeavecung.katabankaccount.usecases;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class AUserCreateABankAccountTest {
+
+    @Test
+    void given_a_connected_user_when_he_create_an_account_without_a_first_deposit_then_the_account_is_created_without_operations() {
+        fail();
+    }
+
+    @Test
+    void given_a_connected_user_when_he_create_an_account_with_a_first_deposit_of_10_euros_then_the_account_is_created_with_a_deposit_operation_of_10_euros() {
+        fail();
+    }
+
+    @Test
+    void given_a_connected_user_when_he_create_an_account_with_a_first_deposit_of_negative_10_euros_then_he_obtains_an_error() {
+        fail();
+    }
+
 }
